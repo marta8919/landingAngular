23d6e7abcf9e1f7fd43e833a05aa40e32e2eeb58
Marta <marta.gilabert.guzman@gmail.com>
2025-03-06 16:51:45 +0100

    basic features working

---
+++ src/app/tasks/new-task/new-task.component.ts
@@ -0,0 +1,37 @@
+import { Component, EventEmitter, inject, Input, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { TasksService } from '../tasks.service';
+
+@Component({
+  selector: 'app-new-task',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './new-task.component.html',
+  styleUrl: './new-task.component.css',
+})
+export class NewTaskComponent {
+  @Output() close = new EventEmitter<void>();
+  @Input({ required: true }) userId!: string;
+
+  enteredTitle = '';
+  enteredSummary = '';
+  enteredDueDate = '';
+
+  private taskService = inject(TasksService);
+
+  onClose() {
+    this.close.emit();
+  }
+
+  onSubmit() {
+    this.taskService.addTask(
+      {
+        title: this.enteredTitle,
+        summary: this.enteredSummary,
+        dueDate: this.enteredDueDate,
+      },
+      this.userId
+    );
+    this.close.emit();
+  }
+}
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,14 +1,17 @@
 import { Component, Output, EventEmitter, Input } from '@angular/core';
 import { User } from './user.model';
+import { CardComponent } from "../ui/card/card.component";
 
 @Component({
   selector: 'app-user',
   standalone: true,
   templateUrl: './user.component.html',
   styleUrl: './user.component.css',
+  imports: [CardComponent],
 })
 export class UserComponent {
   @Output() select = new EventEmitter<string>();
+  @Input({ required: true }) selected!: boolean;
 
   @Input({ required: true })
   user!: User;
